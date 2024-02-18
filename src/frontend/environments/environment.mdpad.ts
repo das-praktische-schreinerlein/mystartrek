57cdad7a5eb2cc45940a11a9c2365e6d229dfb50
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2024-02-18 11:02:56 +0100

    - now bases on mysimplehomepage-5.1.0

--- src/frontend/environments/environment.mdpad.ts
+++ src/frontend/environments/environment.mdpad.ts
@@ -1,9 +1,11 @@
 import {AppEnvironment} from './app-environment';
 import {environment as viewerEnvironment} from '../environments/environment.viewer';
-import {JsPdfGenerator} from '@dps/mycms-frontend-commons/dist/angular-commons/services/jspdf.generator';
+import {
+    PrintDialogPdfGenerator
+} from '@dps/mycms-frontend-commons/dist/angular-commons/services/print-dialog-pdf.generator';
 
 
 export const environment: AppEnvironment = viewerEnvironment;
 
 // TODO if you want pdf replace PrintDialogPdfGenerator by JsPdfGenerator and move jspdf in package.json from optional to dep
-export class EnvironmentPdfGenerator extends JsPdfGenerator {}
+export class EnvironmentPdfGenerator extends PrintDialogPdfGenerator {}
